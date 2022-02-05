70cf256c60db5d7e17661a2d838d14471f759493
Water.Li <lizongbin@hotmail.com>
2022-02-05 18:00:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,34 +11,56 @@ import (
 
 func main() {
 	// Schema
-	fields := graphql.Fields{
+	queryFields := graphql.Fields{
 		"hello": &graphql.Field{
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				return "world", nil
 			},
 		},
+		"hello2": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world2", nil
+			},
+		},
 	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+
+	mutationFields := graphql.Fields{
+		"login": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+		"logout": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world2", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: queryFields}
+	rootMutation := graphql.ObjectConfig{Name: "RootMutation", Fields: mutationFields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery), Mutation: graphql.NewObject(rootMutation)}
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
 	}
 
 	// Query
-	query := `
-		{
-			hello
-		}
-	`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-	}
-	rJSON, _ := json.Marshal(r)
-	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
+	// query := `
+	// 	{
+	// 		hello
+	// 	}
+	// `
+	// params := graphql.Params{Schema: schema, RequestString: query}
+	// r := graphql.Do(params)
+	// if len(r.Errors) > 0 {
+	// 	log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	// }
+	// rJSON, _ := json.Marshal(r)
+	// fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
 
 	h := handler.New(&handler.Config{
 		Schema:   &schema,
@@ -48,6 +69,7 @@ func main() {
 	})
 
 	http.Handle("/graphql", h)
+	fmt.Println("Running a GraphQL API server at http://localhost:8080/graphql")
 	err2 := http.ListenAndServe(":8080", nil)
 	if err2 != nil {
 		fmt.Printf("启动失败:%s", err2)
