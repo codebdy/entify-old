f9b34ccefe79440a368257347f05af6ea214efa4
Water.Li <lizongbin@hotmail.com>
2022-02-15 10:31:44 +0800

    backup

--- schema/query.go
+++ schema/query.go
@@ -5,11 +5,28 @@ import (
 
 	"github.com/graphql-go/graphql"
 	"rxdrag.com/entity-engine/model"
+	"rxdrag.com/entity-engine/utils"
 )
 
 func AppendEntityToQueryFields(entity model.EntityMeta, feilds *graphql.Fields) {
-	(*feilds)[entity.Name] = &graphql.Field{
+	(*feilds)[utils.FirstLower(entity.Name)] = &graphql.Field{
 		Type: graphql.String,
+		// 	Args: graphql.FieldConfigArgument{
+		// 		"distinctOn": &graphql.ArgumentConfig{
+		// 			Type: metaDistinctType,
+		// 		},
+		// 		"limit": &graphql.ArgumentConfig{
+		// 			Type: graphql.Int,
+		// 		},
+		// 		"offset": &graphql.ArgumentConfig{
+		// 			Type: graphql.Int,
+		// 		},
+		// 		"orderBy": &graphql.ArgumentConfig{
+		// 			Type: graphql.String,
+		// 		},
+		// 		"where": &graphql.ArgumentConfig{
+		// 			Type: metaBoolExp,
+		// 		},
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			fmt.Println(p.Context.Value("data"))
 			return "world", nil
