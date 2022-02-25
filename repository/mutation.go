b96278ef1c6b5afb186ba18e50f87e5b97fd00a9
Water.Li <lizongbin@hotmail.com>
2022-02-25 11:29:03 +0800

    backup

--- repository/mutation.go
+++ repository/mutation.go
@@ -4,12 +4,11 @@ import (
 	"github.com/graphql-go/graphql"
 	"rxdrag.com/entity-engine/consts"
 	"rxdrag.com/entity-engine/meta"
-	"rxdrag.com/entity-engine/utils"
 )
 
 func PostOneResolveFn(entity *meta.Entity) graphql.FieldResolveFn {
 	return func(p graphql.ResolveParams) (interface{}, error) {
-		object := p.Args[consts.ARG_OBJECT]
-		return SaveOneEntity(object.(utils.SimpleJSON), entity)
+		object := p.Args[consts.ARG_OBJECT].(map[string]interface{})
+		return SaveOneEntity(object, entity)
 	}
 }
--- repository/repository.go
+++ repository/repository.go
@@ -6,10 +6,9 @@ import (
 
 	"rxdrag.com/entity-engine/config"
 	"rxdrag.com/entity-engine/meta"
-	"rxdrag.com/entity-engine/utils"
 )
 
-func SaveOneEntity(object utils.SimpleJSON, entity *meta.Entity) (interface{}, error) {
+func SaveOneEntity(object map[string]interface{}, entity *meta.Entity) (interface{}, error) {
 	fmt.Println(object)
 	db, err := sql.Open("mysql", config.MYSQL_CONFIG)
 	defer db.Close()
@@ -17,6 +16,18 @@ func SaveOneEntity(object utils.SimpleJSON, entity *meta.Entity) (interface{}, e
 		fmt.Println(err)
 		return nil, err
 	}
+
+	result, err := db.Exec("INSERT INTO `user`(`name`,`password`) VALUES('tom', 'tom')")
+	if err != nil {
+		fmt.Println("insert data failed:", err.Error())
+		return nil, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		fmt.Println("fetch last insert id failed:", err.Error())
+		return nil, err
+	}
+	fmt.Println("insert new record", id)
 	return nil, nil
 	//fmt.Println(p.Args)
 	//fmt.Println(p.Conte
