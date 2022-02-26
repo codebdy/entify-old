d5da6b1fbaef5c2c6678a79f01646d22a560f3f7
Water.Li <lizongbin@hotmail.com>
2022-02-26 21:45:42 +0800

    Update query.go

--- repository/query.go
+++ repository/query.go
@@ -11,6 +11,52 @@ import (
 	"rxdrag.com/entity-engine/utils"
 )
 
+func makeValues(entity *meta.Entity) []interface{} {
+	names := entity.ColumnNames()
+	values := make([]interface{}, len(names))
+	for i, columnName := range names {
+		column := entity.GetColumn(columnName)
+		if column.Type == meta.COLUMN_SIMPLE_JSON {
+			var value utils.SimpleJSON
+			values[i] = &value
+		} else {
+			var value sql.NullString
+			values[i] = &value
+		}
+	}
+
+	return values
+}
+
+func Query(entity *meta.Entity, queryStr string) ([]interface{}, error) {
+	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	rows, err := db.Query(queryStr)
+	columns, err := rows.Columns()
+	var instances []interface{}
+	for rows.Next() {
+		row := make(map[string]interface{})
+		values := makeValues(entity)
+		err = rows.Scan(values...)
+		for i, value := range values {
+			row[columns[i]] = value
+		}
+		instances = append(instances, row)
+	}
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	//fmt.Println(p.Args)
+	//fmt.Println(p.Context.Value("data"))
+	return instances, nil
+}
+
 func QueryOneById(entity *meta.Entity, id int64) (interface{}, error) {
 	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
 	defer db.Close()
@@ -24,18 +70,7 @@ func QueryOneById(entity *meta.Entity, id int64) (interface{}, error) {
 	queryStr = fmt.Sprintf(queryStr, strings.Join(names, ","), entity.GetTableName())
 
 	object := make(map[string]interface{})
-	values := make([]interface{}, len(names))
-	for i, columnName := range names {
-		column := entity.GetColumn(columnName)
-		if column.Type == meta.COLUMN_SIMPLE_JSON {
-			var value utils.SimpleJSON
-			values[i] = &value
-		} else {
-			var value string
-			values[i] = &value
-		}
-	}
-
+	values := makeValues(entity)
 	err = db.QueryRow(queryStr, id).Scan(values...)
 	if err != nil {
 		fmt.Println(err)
@@ -52,46 +87,10 @@ func QueryOneById(entity *meta.Entity, id int64) (interface{}, error) {
 
 func QueryResolveFn(entity *meta.Entity) graphql.FieldResolveFn {
 	return func(p graphql.ResolveParams) (interface{}, error) {
-		db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
-		defer db.Close()
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
 		queryStr := "select * from %s"
 
 		queryStr = fmt.Sprintf(queryStr, entity.GetTableName())
 		//err = db.Select(&instances, queryStr)
-		rows, err := db.Query(queryStr)
-		columns, err := rows.Columns()
-		var instances []utils.SimpleJSON
-		for rows.Next() {
-			row := make(map[string]interface{})
-			values := make([]interface{}, len(columns))
-			for i, columnName := range columns {
-				if columnName == "content" {
-					var value utils.SimpleJSON
-					values[i] = &value
-				} else {
-					var value string
-					values[i] = &value
-				}
-
-			}
-			err = rows.Scan(values...)
-			for i, value := range values {
-				row[columns[i]] = value
-			}
-			instances = append(instances, row)
-		}
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-
-		fmt.Println("Resolve entity:" + entity.Name)
-		//fmt.Println(p.Args)
-		//fmt.Println(p.Context.Value("data"))
-		return instances, nil
+		return Query(entity, queryStr)
 	}
 }
