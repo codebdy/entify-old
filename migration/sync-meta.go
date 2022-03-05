9f62fefd2fbe9ab01939deeaa364d698beac9056
Water.Li <lizongbin@hotmail.com>
2022-03-05 20:45:29 +0800

    Update sync-meta.go

--- migration/sync-meta.go
+++ migration/sync-meta.go
@@ -1,8 +1,13 @@
 package migration
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	"rxdrag.com/entity-engine/consts"
+	"rxdrag.com/entity-engine/repository"
+	"rxdrag.com/entity-engine/schema"
+)
 
 func SyncMetaResolve(p graphql.ResolveParams) (interface{}, error) {
-
-	return nil, nil
+	object := p.Args[consts.ARG_OBJECT].(map[string]interface{})
+	return repository.InsertOne(object, &schema.MetaEntity)
 }
