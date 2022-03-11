b91ea677604ba4fc212cc1a11d7cc37c880c96f4
Water.Li <lizongbin@hotmail.com>
2022-03-11 14:18:10 +0800

    Update buildsql.go

--- repository/buildsql.go
+++ repository/buildsql.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 	"strings"
 
+	"rxdrag.com/entity-engine/consts"
 	"rxdrag.com/entity-engine/meta"
+	"rxdrag.com/entity-engine/repository/dialect"
 )
 
 func BuildQuerySQL(entity *meta.Entity, args map[string]interface{}) (string, []interface{}) {
 	var params []interface{}
 	names := entity.ColumnNames()
-	//sqlBuilder := dialect.GetSQLBuilder()
+	sqlBuilder := dialect.GetSQLBuilder()
+	queryStr := "select %s from %s order by id desc "
+	if args[consts.ARG_WHERE] != nil {
+		whereStr, whereParams := sqlBuilder.BuildBoolExp(args[consts.ARG_WHERE].(map[string]interface{}))
+		queryStr = queryStr + " " + whereStr
+		params = append(params, whereParams...)
+	}
 
-	//whereStr, whereParams := sqlBuilder.BuildBoolExp(args[consts.ARG_WHERE])
-
-	queryStr := "select %s from %s order by id desc"
 	queryStr = fmt.Sprintf(queryStr, strings.Join(names, ","), entity.GetTableName())
 
 	return queryStr, params
