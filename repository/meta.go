82731a5d3c5b3f592569b6fd96f7f34ed440fe17
Water.Li <lizongbin@hotmail.com>
2022-03-12 20:29:40 +0800

    Update meta.go

--- repository/meta.go
+++ repository/meta.go
@@ -1,26 +1,30 @@
 package repository
 
-import "rxdrag.com/entity-engine/meta"
+import (
+	"fmt"
+
+	"rxdrag.com/entity-engine/meta"
+)
 
 func DeleteRelation(relatin *meta.Relation) {
-
+	fmt.Println("Not implement DeleteRelation")
 }
 
 func DeleteEntity(entityName string) {
-
+	fmt.Println("Not implement DeleteEntity")
 }
 
 func AddEntity(entity *meta.Entity) {
-
+	fmt.Println("Not implement AddEntity")
 }
 
 func AddRelation(relation *meta.Relation) {
-
+	fmt.Println("Not implement AddRelation")
 }
 func ModifyEntity(entityDiff *meta.EntityDiff) {
-
+	fmt.Println("Not implement ModifyEntity")
 }
 
 func ModifyRelation(relationDiff *meta.RelationDiff) {
-
+	fmt.Println("Not implement ModifyRelation")
 }
