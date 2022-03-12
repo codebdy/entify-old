8b573e1fbf7083d85e3baa03f6fedde3f92545c2
Water.Li <lizongbin@hotmail.com>
2022-03-12 09:48:34 +0800

    Update diff.go

--- migration/diff.go
+++ migration/diff.go
@@ -1,12 +1,72 @@
 package migration
 
 import (
+	"rxdrag.com/entity-engine/consts"
 	"rxdrag.com/entity-engine/meta"
 	"rxdrag.com/entity-engine/utils"
 )
 
+func findRelation(uuid string, relations []meta.Relation) *meta.Relation {
+	for _, relation := range relations {
+		if relation.Uuid == uuid {
+			return &relation
+		}
+	}
+
+	return nil
+}
+
+func findEntity(uuid string, entities []meta.Entity) *meta.Entity {
+	for _, entity := range entities {
+		if entity.Uuid == uuid {
+			return &entity
+		}
+	}
+
+	return nil
+}
+
 func CreateDiff(published, next utils.Object) *meta.Diff {
 	var diff meta.Diff
+
+	publishedRelations := published[consts.META_RELATIONS].([]meta.Relation)
+	nextRelations := next[consts.META_RELATIONS].([]meta.Relation)
+	for _, relation := range publishedRelations {
+		foundRelation := findRelation(relation.Uuid, nextRelations)
+		//删除的Relation
+		if foundRelation == nil {
+			diff.DeleteRelations = append(diff.DeleteRelations, relation)
+		}
+	}
+	for _, relation := range nextRelations {
+		foundRelation := findRelation(relation.Uuid, publishedRelations)
+		//添加的Relation
+		if foundRelation == nil {
+			diff.AddRlations = append(diff.AddRlations, relation)
+		} else {
+			//此处处理变更的Relation
+		}
+	}
+
+	publishedEntities := published[consts.META_ENTITIES].([]meta.Entity)
+	nextEntities := next[consts.META_ENTITIES].([]meta.Entity)
+
+	for _, entity := range publishedEntities {
+		foundEntity := findEntity(entity.Uuid, nextEntities)
+		//删除的Entity
+		if foundEntity == nil {
+			diff.DeleteEntities = append(diff.DeleteEntities, entity)
+		}
+	}
+	for _, entity := range nextEntities {
+		foundEntity := findEntity(entity.Uuid, publishedEntities)
+		//添加的Entity
+		if foundEntity == nil {
+			diff.AddEntities = append(diff.AddEntities, entity)
+		} else {
+			//此处处理变更的Entity
+		}
+	}
 
 	return &diff
 }
