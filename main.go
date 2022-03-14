cf3ed5c4f59438b01c5eaec7b2451686963f72d2
Water.Li <lizongbin@hotmail.com>
2022-03-14 11:43:17 +0800

    add schema.go

--- main.go
+++ main.go
@@ -2,90 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/graphql-go/graphql"
 	"rxdrag.com/entity-engine/authentication"
-	"rxdrag.com/entity-engine/authentication/jwt"
-	"rxdrag.com/entity-engine/consts"
 	"rxdrag.com/entity-engine/handler"
-	"rxdrag.com/entity-engine/meta"
-	"rxdrag.com/entity-engine/repository"
-	"rxdrag.com/entity-engine/resolve"
 	"rxdrag.com/entity-engine/schema"
 )
 
 func main() {
-	queryFields := graphql.Fields{}
-
-	for _, entity := range *repository.Entities {
-		schema.AppendToQueryFields(entity, &queryFields)
-	}
-
-	mutationFields := graphql.Fields{
-		consts.LOGIN: &graphql.Field{
-			Type: graphql.String,
-			Args: graphql.FieldConfigArgument{
-				consts.LOGIN_NAME: &graphql.ArgumentConfig{
-					Type: &graphql.NonNull{OfType: graphql.String},
-				},
-				consts.PASSWORD: &graphql.ArgumentConfig{
-					Type: &graphql.NonNull{OfType: graphql.String},
-				},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				loginName, err := authentication.Login(p.Args[consts.LOGIN_NAME].(string), p.Args[consts.PASSWORD].(string))
-				if err != nil {
-					return "", err
-				}
-				return jwt.GenerateToken(loginName)
-			},
-		},
-		consts.LOGOUT: &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world2", nil
-			},
-		},
-		consts.PUBLISH: &graphql.Field{
-			Type:    schema.OutputType(&meta.MetaEntity),
-			Resolve: resolve.PublishMetaResolve,
-		},
-		consts.ROLLBACK: &graphql.Field{
-			Type:    schema.OutputType(&meta.MetaEntity),
-			Resolve: resolve.SyncMetaResolve,
-		},
-		consts.SYNC_META: &graphql.Field{
-			Type:    schema.OutputType(&meta.MetaEntity),
-			Resolve: resolve.SyncMetaResolve,
-		},
-	}
-
-	for _, entity := range *repository.Entities {
-		schema.AppendToMutationFields(entity, &mutationFields)
-	}
-
-	rootQuery := graphql.ObjectConfig{Name: consts.ROOT_QUERY_NAME, Fields: queryFields}
-	rootMutation := graphql.ObjectConfig{Name: consts.ROOT_MUTATION_NAME, Fields: mutationFields}
-	schemaConfig := graphql.SchemaConfig{
-		Query:    graphql.NewObject(rootQuery),
-		Mutation: graphql.NewObject(rootMutation),
-		Directives: []*graphql.Directive{
-			graphql.NewDirective(graphql.DirectiveConfig{
-				Name:      "testDirective",
-				Locations: []string{graphql.DirectiveLocationField},
-			}),
-		},
-	}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
-
 	h := handler.New(&handler.Config{
-		Schema: &schema,
+		Schema: schema.GQLSchema,
 		Pretty: true,
 	})
 
@@ -95,5 +22,4 @@ func main() {
 	if err2 != nil {
 		fmt.Printf("启动失败:%s", err2)
 	}
-
 }
---
+++ schema/schema.go
@@ -0,0 +1,89 @@
+package schema
+
+import (
+	"log"
+
+	"github.com/graphql-go/graphql"
+	"rxdrag.com/entity-engine/authentication"
+	"rxdrag.com/entity-engine/authentication/jwt"
+	"rxdrag.com/entity-engine/consts"
+	"rxdrag.com/entity-engine/meta"
+	"rxdrag.com/entity-engine/repository"
+	"rxdrag.com/entity-engine/resolve"
+)
+
+var GQLSchema *graphql.Schema
+
+func InitSchema() {
+	queryFields := graphql.Fields{}
+
+	for _, entity := range *repository.Entities {
+		AppendToQueryFields(entity, &queryFields)
+	}
+
+	mutationFields := graphql.Fields{
+		consts.LOGIN: &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				consts.LOGIN_NAME: &graphql.ArgumentConfig{
+					Type: &graphql.NonNull{OfType: graphql.String},
+				},
+				consts.PASSWORD: &graphql.ArgumentConfig{
+					Type: &graphql.NonNull{OfType: graphql.String},
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				loginName, err := authentication.Login(p.Args[consts.LOGIN_NAME].(string), p.Args[consts.PASSWORD].(string))
+				if err != nil {
+					return "", err
+				}
+				return jwt.GenerateToken(loginName)
+			},
+		},
+		consts.LOGOUT: &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world2", nil
+			},
+		},
+		consts.PUBLISH: &graphql.Field{
+			Type:    OutputType(&meta.MetaEntity),
+			Resolve: resolve.PublishMetaResolve,
+		},
+		consts.ROLLBACK: &graphql.Field{
+			Type:    OutputType(&meta.MetaEntity),
+			Resolve: resolve.SyncMetaResolve,
+		},
+		consts.SYNC_META: &graphql.Field{
+			Type:    OutputType(&meta.MetaEntity),
+			Resolve: resolve.SyncMetaResolve,
+		},
+	}
+
+	for _, entity := range *repository.Entities {
+		AppendToMutationFields(entity, &mutationFields)
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: consts.ROOT_QUERY_NAME, Fields: queryFields}
+	rootMutation := graphql.ObjectConfig{Name: consts.ROOT_MUTATION_NAME, Fields: mutationFields}
+	schemaConfig := graphql.SchemaConfig{
+		Query:    graphql.NewObject(rootQuery),
+		Mutation: graphql.NewObject(rootMutation),
+		Directives: []*graphql.Directive{
+			graphql.NewDirective(graphql.DirectiveConfig{
+				Name:      "testDirective",
+				Locations: []string{graphql.DirectiveLocationField},
+			}),
+		},
+	}
+	theSchema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	GQLSchema = &theSchema
+}
+
+func init() {
+	InitSchema()
+}
