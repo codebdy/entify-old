511e6e43846e84c816ebffcd077bc19539c768b9
Water.Li <lizongbin@hotmail.com>
2022-03-14 10:17:26 +0800

    backup

--- repository/metas.go
+++ repository/metas.go
@@ -1,10 +1,42 @@
 package repository
 
-import "rxdrag.com/entity-engine/meta"
+import (
+	"github.com/mitchellh/mapstructure"
+	"rxdrag.com/entity-engine/consts"
+	"rxdrag.com/entity-engine/meta"
+	"rxdrag.com/entity-engine/utils"
+)
 
-var Entities = &[]*meta.Entity{
-	&meta.MetaStatusEnum,
-	&meta.MetaEntity,
+var Entities *[]*meta.Entity
+
+func QueryPublishedMeta() interface{} {
+	publishedMeta, err := QueryOne(&meta.MetaEntity, QueryArg{
+		consts.ARG_WHERE: QueryArg{
+			consts.META_STATUS: QueryArg{
+				consts.AEG_EQ: meta.META_STATUS_PUBLISHED,
+			},
+		},
+	})
+	if err != nil {
+		panic("Read published meta error" + err.Error())
+	}
+
+	return publishedMeta
+}
+
+func QueryNextMeta() interface{} {
+	nextMeta, err := QueryOne(&meta.MetaEntity, QueryArg{
+		consts.ARG_WHERE: QueryArg{
+			consts.META_STATUS: QueryArg{
+				consts.ARG_ISNULL: true,
+			},
+		},
+	})
+	if err != nil {
+		panic("Read next meta error" + err.Error())
+	}
+
+	return nextMeta
 }
 
 func GetEntityByUuid(uuid string) *meta.Entity {
@@ -15,4 +47,33 @@ func GetEntityByUuid(uuid string) *meta.Entity {
 	}
 
 	return nil
+}
+
+func DecodeContent(obj interface{}) *meta.MetaContent {
+	content := meta.MetaContent{}
+	if obj != nil {
+		err := mapstructure.Decode(obj.(utils.Object)[consts.META_CONTENT], &content)
+		if err != nil {
+			panic("Decode content failure:" + err.Error())
+		}
+	}
+	return &content
+}
+
+func LoadMetas() {
+	publishedMeta := QueryPublishedMeta()
+	publishedContent := DecodeContent(publishedMeta)
+
+	theEntities := make([]*meta.Entity, len(publishedContent.Entities)+2)
+	theEntities[0] = &meta.MetaStatusEnum
+	theEntities[1] = &meta.MetaEntity
+	for i := range publishedContent.Entities {
+		theEntities[i+2] = &publishedContent.Entities[i]
+	}
+
+	Entities = &theEntities
+}
+
+func init() {
+	LoadMetas()
 }
--- resolve/publish.go
+++ resolve/publish.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/graphql-go/graphql"
-	"github.com/mitchellh/mapstructure"
 	"rxdrag.com/entity-engine/consts"
 	"rxdrag.com/entity-engine/meta"
 	"rxdrag.com/entity-engine/migration"
@@ -12,44 +11,16 @@ import (
 	"rxdrag.com/entity-engine/utils"
 )
 
-func decodeContent(obj interface{}) *meta.MetaContent {
-	content := meta.MetaContent{}
-	if obj != nil {
-		err := mapstructure.Decode(obj.(utils.Object)[consts.META_CONTENT], &content)
-		if err != nil {
-			panic("Decode content failure:" + err.Error())
-		}
-	}
-	return &content
-}
-
 func PublishMetaResolve(p graphql.ResolveParams) (interface{}, error) {
-	publishedMeta, err := repository.QueryOne(&meta.MetaEntity, repository.QueryArg{
-		consts.ARG_WHERE: repository.QueryArg{
-			consts.META_STATUS: repository.QueryArg{
-				consts.AEG_EQ: meta.META_STATUS_PUBLISHED,
-			},
-		},
-	})
-	if err != nil {
-		panic("Read published meta error" + err.Error())
-	}
-	nextMeta, err := repository.QueryOne(&meta.MetaEntity, repository.QueryArg{
-		consts.ARG_WHERE: repository.QueryArg{
-			consts.META_STATUS: repository.QueryArg{
-				consts.ARG_ISNULL: true,
-			},
-		},
-	})
-	if err != nil {
-		panic("Read next meta error" + err.Error())
-	}
+	publishedMeta := repository.QueryPublishedMeta()
+	nextMeta := repository.QueryNextMeta()
 
 	if nextMeta == nil {
 		panic("Can not find unpublished meta")
 	}
-	publishedContent := decodeContent(publishedMeta)
-	nextContent := decodeContent(nextMeta)
+	publishedContent := repository.DecodeContent(publishedMeta)
+	nextContent := repository.DecodeContent(nextMeta)
+	migration.ValidateNextMeta(nextContent)
 	diff := migration.CreateDiff(publishedContent, nextContent)
 	migration.ExcuteDiff(diff)
 	metaObj := nextMeta.(utils.Object)
