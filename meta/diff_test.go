7f62da4fbeb2a03b391fa3cc9d843edbe623df09
Water.Li <lizongbin@hotmail.com>
2022-03-17 17:08:06 +0800

    Update diff_test.go

--- meta/diff_test.go
+++ meta/diff_test.go
@@ -1,7 +1,79 @@
 package meta
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestModifyEntityName(t *testing.T) {
+	oldData := `
+	{
+		"entities": [
+			{
+				"name": "User",
+				"uuid": "3e9ae743-de18-4b0c-a77e-3726be4049a8",
+				"columns": [
+					{
+						"name": "id",
+						"type": "ID",
+						"uuid": "6758ae89-1e2c-462d-907c-a54baf6cf6fd",
+						"primary": true
+					},
+					{
+						"name": "newColumn1",
+						"type": "String",
+						"uuid": "e1afb0c4-5eee-40f3-8c34-3ce15746877b"
+					}
+				],
+				"entityType": "Normal"
+			}
+		],
+		"relations": []
+	}
+	`
 
+	newData := `
+	{
+		"entities": [
+			{
+				"name": "User2",
+				"uuid": "3e9ae743-de18-4b0c-a77e-3726be4049a8",
+				"columns": [
+					{
+						"name": "id",
+						"type": "ID",
+						"uuid": "6758ae89-1e2c-462d-907c-a54baf6cf6fd",
+						"primary": true
+					},
+					{
+						"name": "newColumn1",
+						"type": "String",
+						"uuid": "e1afb0c4-5eee-40f3-8c34-3ce15746877b"
+					}
+				],
+				"entityType": "Normal"
+			}
+		],
+		"relations": []
+	}
+	`
+
+	oldM := MetaContent{}
+	json.Unmarshal([]byte(oldData), &oldM)
+	newM := MetaContent{}
+	json.Unmarshal([]byte(newData), &newM)
+	newM.Validate()
+	diff := CreateDiff(&oldM, &newM)
+
+	if len(diff.ModifiedTables) != 1 {
+		t.Errorf("Diffent table number is %d ,not 1", len(diff.ModifiedTables))
+	}
+
+	if diff.oldContent.Tables()[0].Name != "user" {
+		t.Errorf("Old name is %s, not expected user", diff.oldContent.Tables()[0].Name)
+	}
+
+	if diff.newContent.Tables()[0].Name != "user2" {
+		t.Errorf("Old name is %s, not expected user2", diff.newContent.Tables()[0].Name)
+	}
 }
