ea07e6aad37da620742ab1b5ab52da5dc58f10c4
Water.Li <lizongbin@hotmail.com>
2022-03-23 16:29:38 +0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -56,13 +56,34 @@ func NewModel(c *meta.MetaContent) *Model {
 		Relations:  []*Relation{},
 		Tables:     entityTables(c),
 	}
-
-	model.buildEntities(c)
+	model.buildEnums(enums)
+	model.buildInterfaces(interfaces)
+	model.buildEntities(entities)
 	model.buildRelations(c)
-	model.buildInheritEntities()
+	model.buildInherits()
 	model.buildInheritRelations()
 	model.buildTables()
 	return &model
+}
+
+func (model *Model) buildEnums(metas []*meta.EntityMeta) {
+	for i := range metas {
+		model.Enums[i] = &Enum{
+			EntityMeta: *metas[i],
+			model:      model,
+		}
+	}
+}
+
+func (model *Model) buildInterfaces(metas []*meta.EntityMeta) {
+	for i := range metas {
+		model.Interfaces[i] = &Interface{
+			EntityMeta:   *metas[i],
+			Associations: []*Association{},
+			Children:     []*Entity{},
+			model:        model,
+		}
+	}
 }
 
 func (model *Model) buildEntities(metas []*meta.EntityMeta) {
@@ -72,6 +93,37 @@ func (model *Model) buildEntities(metas []*meta.EntityMeta) {
 			Interfaces:   []*Interface{},
 			Associations: []*Association{},
 			model:        model,
+		}
+	}
+}
+
+func (model *Model) buildRelations(c *meta.MetaContent) {
+	for i := range c.Relations {
+		relation := c.Relations[i]
+		if relation.RelationType != meta.IMPLEMENTS {
+			model.Relations = append(model.Relations, &Relation{
+				RelationMeta: relation,
+				model:        model,
+			})
+		}
+	}
+}
+
+func (model *Model) buildInherits() {
+	for i := range model.Relations {
+		relation := model.Relations[i]
+		if relation.RelationType == meta.IMPLEMENTS {
+			sourceEntity := model.GetEntityByUuid(relation.SourceId)
+			if sourceEntity == nil {
+				panic("Can not find entity by relation:" + relation.SourceId)
+			}
+			interfaceEntity := model.GetInterfaceByUuid(relation.TargetId)
+			if interfaceEntity == nil {
+				panic("Can not find interface by relation:" + relation.TargetId)
+			}
+
+			sourceEntity.Interfaces = append(sourceEntity.Interfaces, interfaceEntity)
+			interfaceEntity.Children = append(interfaceEntity.Children, sourceEntity)
 		}
 	}
 }
@@ -90,35 +142,24 @@ func (model *Model) buildTables() {
 func (model *Model) buildInheritRelations() {
 }
 
-func (model *Model) buildInheritEntities() {
-	for i := range model.Relations {
-		relation := model.Relations[i]
-		if relation.RelationType == meta.IMPLEMENTS {
-			sourceEntity := model.GetEntityByUuid(relation.SourceId)
-			if sourceEntity == nil {
-				panic("Can not find entity by relation:" + relation.SourceId)
-			}
-			targetEntity := model.GetEntityByUuid(relation.TargetId)
-			if targetEntity == nil {
-				panic("Can not find entity by relation:" + relation.TargetId)
-			}
-
-			sourceEntity.Parent = targetEntity
-			targetEntity.Children = append(targetEntity.Children, sourceEntity)
+func (m *Model) GetEnumByUuid(uuid string) *Enum {
+	for i := range m.Enums {
+		enum := m.Enums[i]
+		if enum.Uuid == uuid {
+			return enum
 		}
 	}
+	return nil
 }
 
-func (model *Model) buildRelations(c *meta.MetaContent) {
-	for i := range c.Relations {
-		relation := c.Relations[i]
-		if relation.RelationType != meta.IMPLEMENTS {
-			model.Relations = append(model.Relations, &Relation{
-				RelationMeta: relation,
-				model:        model,
-			})
+func (m *Model) GetInterfaceByUuid(uuid string) *Interface {
+	for i := range m.Interfaces {
+		intf := m.Interfaces[i]
+		if intf.Uuid == uuid {
+			return intf
 		}
 	}
+	return nil
 }
 
 func (m *Model) GetEntityByUuid(uuid string) *Entity {
