7240add2ab4a4c8dfa1761a5bda9dceef61f9718
Water.Li <lizongbin@hotmail.com>
2022-03-24 17:12:37 +0800

    backup

--- repository/save.go
+++ repository/save.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	"rxdrag.com/entity-engine/config"
 	"rxdrag.com/entity-engine/consts"
@@ -12,36 +11,15 @@ import (
 	"rxdrag.com/entity-engine/utils"
 )
 
-func clearTransaction(tx *sql.Tx) {
-	err := tx.Rollback()
-	if err != sql.ErrTxDone && err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func InsertOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
-	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	tx, err := db.Begin()
-	defer clearTransaction(tx)
-
-	if err != nil {
-		return nil, err
-	}
+func (tx *Tx) InsertOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
 	sqlBuilder := dialect.GetSQLBuilder()
 	saveStr, values := sqlBuilder.BuildInsertSQL(object, entity)
 
-	_, err = tx.Exec(saveStr, values...)
+	_, err := tx.Exec(saveStr, values...)
 	if err != nil {
 		fmt.Println("Insert data failed:", err.Error())
 		return nil, err
 	}
-	tx.Commit()
 
 	id := object[consts.META_ID]
 	savedObject, err := QueryOneById(entity, id)
@@ -58,29 +36,17 @@ func InsertOne(object map[string]interface{}, entity *model.Entity) (interface{}
 	return savedObject, nil
 }
 
-func UpdateOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
-	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
+func (tx *Tx) UpdateOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
+
 	sqlBuilder := dialect.GetSQLBuilder()
 
-	tx, err := db.Begin()
-	defer clearTransaction(tx)
-
 	saveStr, values := sqlBuilder.BuildUpdateSQL(object, entity)
-	if err != nil {
-		return nil, err
-	}
 	fmt.Println(saveStr)
-	_, err = tx.Exec(saveStr, values...)
+	_, err := tx.Exec(saveStr, values...)
 	if err != nil {
 		fmt.Println("Update data failed:", err.Error())
 		return nil, err
 	}
-	tx.Commit()
 
 	id := object[consts.META_ID]
 
@@ -93,10 +59,56 @@ func UpdateOne(object map[string]interface{}, entity *model.Entity) (interface{}
 }
 
 func SaveOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
+	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
+	defer db.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+	tx, err := NewTx(db)
+	defer tx.clear()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	obj, err := tx.doSaveOne(object, entity)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		panic(err.Error())
+	}
+	return obj, nil
+}
+
+func InsertOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
+	db, err := sql.Open(config.DRIVER_NAME, config.MYSQL_CONFIG)
+	defer db.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+	tx, err := NewTx(db)
+	defer tx.clear()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	obj, err := tx.InsertOne(object, entity)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		panic(err.Error())
+	}
+	return obj, nil
+}
+
+func (tx *Tx) doSaveOne(object map[string]interface{}, entity *model.Entity) (interface{}, error) {
 	if object[consts.META_ID] == nil {
 		object[consts.META_ID] = utils.CreateId()
-		return InsertOne(object, entity)
+		return tx.InsertOne(object, entity)
 	} else {
-		return UpdateOne(object, entity)
+		return tx.UpdateOne(object, entity)
 	}
 }
