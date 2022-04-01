45f2ce22221ca47fa6b864275ac688be0cab7418
Water.Li <lizongbin@hotmail.com>
2022-04-01 20:52:40 +0800

    backup

--- model/domain/association.go
+++ model/domain/association.go
@@ -1,11 +1,9 @@
 package domain
 
-import "rxdrag.com/entity-engine/model/meta"
-
 type Association struct {
 	Name        string
-	Relation    *meta.RelationMeta
-	OfEntity    *Class
-	TypeEntity  *Class
+	Relation    *Relation
+	OwnerClass  *Class
+	TypeClass   *Class
 	Description string
 }
---
+++ model/domain/relation.go
@@ -0,0 +1,7 @@
+package domain
+
+import "rxdrag.com/entity-engine/model/meta"
+
+type Relation struct {
+	meta.RelationMeta
+}
