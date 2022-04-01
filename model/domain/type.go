74cc08f658af2fc238925d2a1a384e36c9e7c8de
Water.Li <lizongbin@hotmail.com>
2022-04-01 16:24:48 +0800

    Create type.go

---
+++ model/domain/type.go
@@ -0,0 +1,21 @@
+package domain
+
+const (
+	ID                 string = "ID"
+	INT                string = "Int"
+	FLOAT              string = "Float"
+	BOOLEAN            string = "Boolean"
+	STRING             string = "String"
+	DATE               string = "Date"
+	ENUM               string = "Enum"
+	VALUE_OBJECT       string = "ValueObject"
+	ENTITY             string = "Entity"
+	ID_ARRAY           string = "ID[]"
+	INT_ARRAY          string = "Int[]"
+	FLOAT_ARRAY        string = "Float[]"
+	STRING_ARRAY       string = "String[]"
+	DATE_ARRAY         string = "Date[]"
+	ENUM_ARRAY         string = "EnumArray"
+	VALUE_OBJECT_ARRAY string = "ValueObjectArray"
+	ENTITY_ARRAY       string = "EntityArray"
+)
