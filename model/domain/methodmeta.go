c41ba2bd4d43b70345c66ea7d33929382334d2b0
Water.Li <lizongbin@hotmail.com>
2022-04-01 16:11:01 +0800

    Update methodmeta.go

--- model/domain/methodmeta.go
+++ model/domain/methodmeta.go
@@ -7,7 +7,20 @@ const (
 )
 
 type ArgMeta struct {
+	Uuid      string `json:"uuid"`
+	Type      string `json:"type"`
+	Name      string `json:"name"`
+	TypeUuid  string `json:"typeUuid"`
+	TypeLabel string `json:"typeLabel"`
 }
 
 type MethodMeta struct {
+	Uuid             string    `json:"uuid"`
+	Name             string    `json:"name"`
+	Type             string    `json:"type"`
+	TypeUuid         string    `json:"typeUuid"`
+	TypeLabel        string    `json:"typeLabel"`
+	Args             []ArgMeta `json:"args"`
+	ImplementType    string    `json:"implementType"`
+	MethodImplements string    `json:"methodImplements"`
 }
