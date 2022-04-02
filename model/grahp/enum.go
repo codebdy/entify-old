472873bd76bbcc67e84c2658ac233a9d98af10d8
Water.Li <lizongbin@hotmail.com>
2022-04-02 10:05:46 +0800

    backup

--- model/grahp/enum.go
+++ model/grahp/enum.go
@@ -1,4 +1,10 @@
 package grahp
 
+import "github.com/graphql-go/graphql"
+
 type Enum struct {
+}
+
+func (e *Enum) GqlType() *graphql.Enum {
+	return nil
 }
---
+++ model/grahp/node.go
@@ -0,0 +1,8 @@
+package grahp
+
+import "github.com/graphql-go/graphql"
+
+type GqlNode interface {
+	OutputType() graphql.Type
+	InputType() *graphql.InputObject
+}
