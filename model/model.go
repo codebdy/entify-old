2a31a733b2445afbf19a6cba50391ecac967269e
Water.Li <lizongbin@hotmail.com>
2022-04-03 19:47:10 +0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -5,24 +5,23 @@ import (
 	"rxdrag.com/entity-engine/model/domain"
 	"rxdrag.com/entity-engine/model/graph"
 	"rxdrag.com/entity-engine/model/meta"
-	"rxdrag.com/entity-engine/model/table"
 )
 
 type Model struct {
 	Meta   *meta.Model
 	Domain *domain.Model
 	Grahp  *graph.Model
-	Tables []*table.Table
 	Schema *graphql.Schema
 }
 
 func New(c *meta.MetaContent) *Model {
 	metaModel := meta.New(c)
+	domainModel := domain.New(metaModel)
+	grahpModel := graph.New(domainModel)
 	model := Model{
 		Meta:   metaModel,
-		Domain: &domain.Model{},
-		Grahp:  &graph.Model{},
-		Tables: []*table.Table{},
+		Domain: domainModel,
+		Grahp:  grahpModel,
 		Schema: nil,
 	}
 	return &model
