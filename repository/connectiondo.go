56338db0a4f886fc8ff9952851d5472a3d994d82
Water.Li <lizongbin@hotmail.com>
2022-04-18 07:38:19 +0800

    backup

--- repository/connectiondo.go
+++ repository/connectiondo.go
@@ -5,121 +5,10 @@ import (
 	"fmt"
 
 	"rxdrag.com/entity-engine/consts"
-	"rxdrag.com/entity-engine/db"
 	"rxdrag.com/entity-engine/db/dialect"
 	"rxdrag.com/entity-engine/model/data"
 	"rxdrag.com/entity-engine/model/graph"
-	"rxdrag.com/entity-engine/model/meta"
-	"rxdrag.com/entity-engine/utils"
 )
-
-func makeValues(node graph.Noder) []interface{} {
-	names := node.AllAttributeNames()
-	values := make([]interface{}, len(names))
-	for i, attrName := range names {
-		attr := node.GetAttributeByName(attrName)
-		switch attr.Type {
-		case meta.ID:
-			var value db.NullUint64
-			values[i] = &value
-			break
-		case meta.INT:
-			var value sql.NullInt64
-			values[i] = &value
-			break
-		case meta.FLOAT:
-			var value sql.NullFloat64
-			values[i] = &value
-			break
-		case meta.BOOLEAN:
-			var value sql.NullBool
-			values[i] = &value
-			break
-		case meta.DATE:
-			var value sql.NullTime
-			values[i] = &value
-			break
-		case meta.CLASS_VALUE_OBJECT,
-			meta.ID_ARRAY,
-			meta.INT_ARRAY,
-			meta.FLOAT_ARRAY,
-			meta.STRING_ARRAY,
-			meta.DATE_ARRAY,
-			meta.ENUM_ARRAY,
-			meta.VALUE_OBJECT_ARRAY,
-			meta.ENTITY_ARRAY:
-			var value utils.JSON
-			values[i] = &value
-			break
-			// COLUMN_SIMPLE_ARRAY string = "simpleArray" ##待添加代码
-			// COLUMN_JSON_ARRAY   string = "JsonArray"
-		default:
-			var value sql.NullString
-			values[i] = &value
-		}
-	}
-
-	return values
-}
-
-func convertValuesToObject(values []interface{}, node graph.Noder) map[string]interface{} {
-	object := make(map[string]interface{})
-	names := node.AllAttributeNames()
-	for i, value := range values {
-		attrName := names[i]
-		column := node.GetAttributeByName(attrName)
-		switch column.Type {
-		case meta.ID:
-			nullValue := value.(*db.NullUint64)
-			if nullValue.Valid {
-				object[attrName] = nullValue.Uint64
-			}
-			break
-		case meta.INT:
-			nullValue := value.(*sql.NullInt64)
-			if nullValue.Valid {
-				object[attrName] = nullValue.Int64
-			}
-			break
-		case meta.FLOAT:
-			nullValue := value.(*sql.NullFloat64)
-			if nullValue.Valid {
-				object[attrName] = nullValue.Float64
-			}
-			break
-		case meta.BOOLEAN:
-			nullValue := value.(*sql.NullBool)
-			if nullValue.Valid {
-				object[attrName] = nullValue.Bool
-			}
-			break
-		case meta.DATE:
-			nullValue := value.(*sql.NullTime)
-			if nullValue.Valid {
-				object[attrName] = nullValue.Time
-			}
-			break
-		case meta.VALUE_OBJECT,
-			meta.ID_ARRAY,
-			meta.INT_ARRAY,
-			meta.FLOAT_ARRAY,
-			meta.STRING_ARRAY,
-			meta.DATE_ARRAY,
-			meta.ENUM_ARRAY,
-			meta.VALUE_OBJECT_ARRAY,
-			meta.ENTITY_ARRAY:
-			object[attrName] = value
-			break
-		default:
-			nullValue := value.(*sql.NullString)
-			if nullValue.Valid {
-				object[attrName] = nullValue.String
-			}
-		}
-
-	}
-	return object
-}
 
 func (con *Connection) doQueryEntity(node graph.Noder, args map[string]interface{}) ([]interface{}, error) {
 	builder := dialect.GetSQLBuilder()
@@ -135,7 +24,7 @@ func (con *Connection) doQueryEntity(node graph.Noder, args map[string]interface
 	}
 	var instances []interface{}
 	for rows.Next() {
-		values := makeValues(node)
+		values := makeQueryValues(node)
 		err = rows.Scan(values...)
 		instances = append(instances, convertValuesToObject(values, node))
 	}
@@ -164,7 +53,7 @@ func (con *Connection) doQueryOne(node graph.Noder, args map[string]interface{})
 
 	queryStr, params := builder.BuildQuerySQL(node, args)
 
-	values := makeValues(node)
+	values := makeQueryValues(node)
 	err := con.Dbx.QueryRow(queryStr, params...).Scan(values...)
 
 	switch {
@@ -182,7 +71,7 @@ func (con *Connection) doQueryOne(node graph.Noder, args map[string]interface{})
 func (con *Connection) doInsertOne(instance *data.Instance) (map[string]interface{}, error) {
 	sqlBuilder := dialect.GetSQLBuilder()
 	saveStr := sqlBuilder.BuildInsertSQL(instance.Fields, instance.Table())
-	values := data.MakeValues(instance.Fields)
+	values := makeSaveValues(instance.Fields)
 	// for _, association := range entity.AllAssociations() {
 	// 	if object[association.Name()] == nil {
 	// 		continue
@@ -232,7 +121,7 @@ func (con *Connection) doUpdateOne(instance *data.Instance) (map[string]interfac
 	sqlBuilder := dialect.GetSQLBuilder()
 
 	saveStr := sqlBuilder.BuildUpdateSQL(instance.Id, instance.Fields, instance.Table())
-	values := data.MakeValues(instance.Fields)
+	values := makeSaveValues(instance.Fields)
 	fmt.Println(saveStr)
 	_, err := con.Dbx.Exec(saveStr, values...)
 	if err != nil {
---
+++ repository/convert.go
@@ -0,0 +1,142 @@
+package repository
+
+import (
+	"database/sql"
+	"encoding/json"
+
+	"rxdrag.com/entity-engine/db"
+	"rxdrag.com/entity-engine/model/data"
+	"rxdrag.com/entity-engine/model/graph"
+	"rxdrag.com/entity-engine/model/meta"
+	"rxdrag.com/entity-engine/utils"
+)
+
+func makeSaveValues(fields []*data.Field) []interface{} {
+	objValues := make([]interface{}, 0, len(fields))
+	for _, field := range fields {
+		value := field.Value
+		column := field.Column
+
+		if column.Type == meta.VALUE_OBJECT ||
+			column.Type == meta.ID_ARRAY ||
+			column.Type == meta.INT_ARRAY ||
+			column.Type == meta.FLOAT_ARRAY ||
+			column.Type == meta.STRING_ARRAY ||
+			column.Type == meta.DATE_ARRAY ||
+			column.Type == meta.ENUM_ARRAY ||
+			column.Type == meta.VALUE_OBJECT_ARRAY ||
+			column.Type == meta.ENTITY_ARRAY {
+			value, _ = json.Marshal(value)
+		}
+		objValues = append(objValues, value)
+	}
+	return objValues
+}
+
+func makeQueryValues(node graph.Noder) []interface{} {
+	names := node.AllAttributeNames()
+	values := make([]interface{}, len(names))
+	for i, attrName := range names {
+		attr := node.GetAttributeByName(attrName)
+		switch attr.Type {
+		case meta.ID:
+			var value db.NullUint64
+			values[i] = &value
+			break
+		case meta.INT:
+			var value sql.NullInt64
+			values[i] = &value
+			break
+		case meta.FLOAT:
+			var value sql.NullFloat64
+			values[i] = &value
+			break
+		case meta.BOOLEAN:
+			var value sql.NullBool
+			values[i] = &value
+			break
+		case meta.DATE:
+			var value sql.NullTime
+			values[i] = &value
+			break
+		case meta.CLASS_VALUE_OBJECT,
+			meta.ID_ARRAY,
+			meta.INT_ARRAY,
+			meta.FLOAT_ARRAY,
+			meta.STRING_ARRAY,
+			meta.DATE_ARRAY,
+			meta.ENUM_ARRAY,
+			meta.VALUE_OBJECT_ARRAY,
+			meta.ENTITY_ARRAY:
+			var value utils.JSON
+			values[i] = &value
+			break
+			// COLUMN_SIMPLE_ARRAY string = "simpleArray" ##待添加代码
+			// COLUMN_JSON_ARRAY   string = "JsonArray"
+		default:
+			var value sql.NullString
+			values[i] = &value
+		}
+	}
+
+	return values
+}
+
+func convertValuesToObject(values []interface{}, node graph.Noder) map[string]interface{} {
+	object := make(map[string]interface{})
+	names := node.AllAttributeNames()
+	for i, value := range values {
+		attrName := names[i]
+		column := node.GetAttributeByName(attrName)
+		switch column.Type {
+		case meta.ID:
+			nullValue := value.(*db.NullUint64)
+			if nullValue.Valid {
+				object[attrName] = nullValue.Uint64
+			}
+			break
+		case meta.INT:
+			nullValue := value.(*sql.NullInt64)
+			if nullValue.Valid {
+				object[attrName] = nullValue.Int64
+			}
+			break
+		case meta.FLOAT:
+			nullValue := value.(*sql.NullFloat64)
+			if nullValue.Valid {
+				object[attrName] = nullValue.Float64
+			}
+			break
+		case meta.BOOLEAN:
+			nullValue := value.(*sql.NullBool)
+			if nullValue.Valid {
+				object[attrName] = nullValue.Bool
+			}
+			break
+		case meta.DATE:
+			nullValue := value.(*sql.NullTime)
+			if nullValue.Valid {
+				object[attrName] = nullValue.Time
+			}
+			break
+		case meta.VALUE_OBJECT,
+			meta.ID_ARRAY,
+			meta.INT_ARRAY,
+			meta.FLOAT_ARRAY,
+			meta.STRING_ARRAY,
+			meta.DATE_ARRAY,
+			meta.ENUM_ARRAY,
+			meta.VALUE_OBJECT_ARRAY,
+			meta.ENTITY_ARRAY:
+			object[attrName] = value
+			break
+		default:
+			nullValue := value.(*sql.NullString)
+			if nullValue.Valid {
+				object[attrName] = nullValue.String
+			}
+		}
+
+	}
+	return object
+}
